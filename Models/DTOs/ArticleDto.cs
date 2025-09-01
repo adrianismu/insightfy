74c1fdc082520cb4e048d525191cbb4b1e4be96d
adrianismu <adrianismu263@gmail.com>
2025-09-01 10:13:59 +0700

    first commit

---
+++ Models/DTOs/ArticleDto.cs
@@ -0,0 +1,14 @@
+namespace Insightify.Models.DTOs
+{
+    public class ArticleDto
+    {
+        public SourceDto? Source { get; set; }
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public string? UrlToImage { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string? Content { get; set; }
+    }
+}
---
+++ Models/DTOs/SourceDto.cs
@@ -0,0 +1,8 @@
+namespace Insightify.Models.DTOs
+{
+    public class SourceDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
